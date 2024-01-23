3f9c6b9806034d7961c96593ccab46d729628e9b
nicolasmarra <nicolasmarra12@gmail.com>
2024-01-23 17:48:12 +0100

    affichage des paris en live

--- branco.py
+++ branco.py
@@ -20,15 +20,22 @@ intents = discord.Intents.all()
 # création du client Discord
 client = discord.Client(intents=intents)
 
-def get_paris():
+
+def get_paris(commande):
     url = "https://www.betclic.fr/football-s1"
 
+
     response = requests.get(url)
-    
+
     soup = BeautifulSoup(response.text, "html.parser")
 
-    paris_items = soup.find_all(class_="cardEvent ng-star-inserted")
+    paris_items = None 
 
+    if commande == "/paris":
+        paris_items = soup.find_all(class_=["cardEvent ng-star-inserted", "cardEvent is-superLive ng-star-inserted"])
+    elif commande == "/paris-live":
+        paris_items = soup.find_all(class_="cardEvent is-live ng-star-inserted")
+    
     paris = []
 
     for pari_item in paris_items:
@@ -62,6 +69,28 @@ def get_paris():
     return paris
 
 
+def afficher_paris(commande):
+    embed_paris_list = []
+    for pari_info in get_paris(commande):
+        equipe_domicile = pari_info.get("equipe_domicile", "N/A")
+        equipe_exterieur = pari_info.get("equipe_exterieur", "N/A")
+        cote_domicile = pari_info.get("cote_domicile", "N/A")
+        cote_match_nul = pari_info.get("cote_match_nul", "N/A")
+        cote_exterieur = pari_info.get("cote_exterieur", "N/A")
+        type_evenement = pari_info.get("type_evenement", "N/A")
+        evenement_heure = pari_info.get("evenement_heure", "N/A")
+
+        message_paris = (
+            f"Cotes: {equipe_domicile} : {cote_domicile} /  {equipe_exterieur} : {cote_exterieur} / Match Nul : {cote_match_nul}\n"
+            )
+        
+
+        embed_paris = discord.Embed(title=f"**{type_evenement} - {evenement_heure}**\n", color=0xff0000)
+        
+        embed_paris.add_field(name=f"**{equipe_domicile} vs {equipe_exterieur}**", value=message_paris)
+        embed_paris_list.append(embed_paris)
+    
+    return embed_paris_list
 
 # événement au démarrage du bot
 @client.event 
@@ -71,23 +100,13 @@ async def on_ready():
 # événement à la réception d'un message
 @client.event
 async def on_message(message):
-    if message.channel.id == PARIS_CHANNEL_ID and message.content == "/paris":
+    commande = message.content
+    if message.channel.id == PARIS_CHANNEL_ID and (commande == "/paris" or commande == "/paris-live"):
 
-        for pari_info in get_paris():
-            equipe_domicile = pari_info.get("equipe_domicile", "N/A")
-            equipe_exterieur = pari_info.get("equipe_exterieur", "N/A")
-            cote_domicile = pari_info.get("cote_domicile", "N/A")
-            cote_match_nul = pari_info.get("cote_match_nul", "N/A")
-            cote_exterieur = pari_info.get("cote_exterieur", "N/A")
-            type_evenement = pari_info.get("type_evenement", "N/A")
-            evenement_heure = pari_info.get("evenement_heure", "N/A")
+        embed_paris = afficher_paris(commande)
+        for embed_pari in embed_paris:
+            await message.channel.send(embed=embed_pari)
 
-            message_paris = (
-                f"Cotes: {equipe_domicile} : {cote_domicile} /  {equipe_exterieur} : {cote_exterieur} / Match Nul : {cote_match_nul}\n"
-                )
-            embed_paris = discord.Embed(title=f"**{type_evenement} - {evenement_heure}**\n", color=0xff0000)
-            embed_paris.add_field(name=f"**{equipe_domicile} vs {equipe_exterieur}**", value=message_paris)
-            await message.channel.send(embed=embed_paris)
 
 
     if message.content == "/delete" and (message.author == "nicolasmarra" or message.author.guild_permissions.administrator):
